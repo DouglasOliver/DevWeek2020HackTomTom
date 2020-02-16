83694ee050b4a529eb398de15a88e2660171b800
Marie <mcburns2222@gmail.com>
2020-02-15 18:36:55 -0800

    Fix long lat and add missing names

--- stores.js
+++ stores.js
@@ -5,8 +5,8 @@ const stores = {
         "geometry": {
             "type": "Point",
             "coordinates": [
-                37.786390,
-                -122.395130
+                -122.395130,
+                37.786390
             ]
         },
         "properties": {
@@ -15,63 +15,65 @@ const stores = {
             "name": "DragonEats"
         }
     },
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    37.785461,
-                    -122.399048
-                ]
-            },
-            "properties": {
-                "address": "22 Hawthorne St, San Francisco, CA 94105",
-                "city": "San Francisco",
-                "name": "Benu"
-            }
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                -122.399048,
+                37.785461
+            ]
         },
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    37.788528,
-                    -122.395142
-                ]
-            },
-            "properties": {
-                "address": "405 Howard St #310, San Francisco, CA 94105",
-                "city": "San Francisco",
-                "name": "Spice Kit"
-            }
-        },
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    37.789490,
-                    -122.397590
-                ]
-            },
-            "properties": {
-                "address": "100 First Plaza, 100 1st St #110, San Francisco, CA 94105",
-                "city": "San Francisco"
-            }
-        },
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    37.789490,
-                    -122.397590
-                ]
-            },
-            "properties": {
-                "address": "255 Kearny St, San Francisco, CA 94108",
-                "city": "San Francisco"
-            }
+        "properties": {
+            "address": "22 Hawthorne St, San Francisco, CA 94105",
+            "city": "San Francisco",
+            "name": "Benu"
         }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                -122.395142,
+                37.788528
+            ]
+        },
+        "properties": {
+            "address": "405 Howard St #310, San Francisco, CA 94105",
+            "city": "San Francisco",
+            "name": "Spice Kit"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                -122.397590,
+                37.789490
+            ]
+        },
+        "properties": {
+            "address": "100 First Plaza, 100 1st St #110, San Francisco, CA 94105",
+            "city": "San Francisco",
+            "name": "Oasis Grill"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                -122.397590,
+                37.789490
+            ]
+        },
+        "properties": {
+            "address": "255 Kearny St, San Francisco, CA 94108",
+            "city": "San Francisco",
+            "name": "SF Wraps"
+        }
+    }
     ]
 };
