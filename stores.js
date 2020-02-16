afcf68fb435bd67182dda72c3813bfa8d8f2277a
Marie <mcburns2222@gmail.com>
2020-02-15 17:43:03 -0800

    Create stores.js

---
+++ stores.js
@@ -0,0 +1,77 @@
+const stores = {
+    "type": "FeatureCollection",
+    "features": [{
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                37.786390,
+                -122.395130
+            ]
+        },
+        "properties": {
+            "address": "555 Folsom St, San Francisco, CA 94105",
+            "city": "San Francisco",
+            "name": "DragonEats"
+        }
+    },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    37.785461,
+                    -122.399048
+                ]
+            },
+            "properties": {
+                "address": "22 Hawthorne St, San Francisco, CA 94105",
+                "city": "San Francisco",
+                "name": "Benu"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    37.788528,
+                    -122.395142
+                ]
+            },
+            "properties": {
+                "address": "405 Howard St #310, San Francisco, CA 94105",
+                "city": "San Francisco",
+                "name": "Spice Kit"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    37.789490,
+                    -122.397590
+                ]
+            },
+            "properties": {
+                "address": "100 First Plaza, 100 1st St #110, San Francisco, CA 94105",
+                "city": "San Francisco"
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    37.789490,
+                    -122.397590
+                ]
+            },
+            "properties": {
+                "address": "255 Kearny St, San Francisco, CA 94108",
+                "city": "San Francisco"
+            }
+        }
+    ]
+};
